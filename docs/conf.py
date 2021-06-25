326811dbe95b510ab6dc21b41818841c5ab51747
veit <kontakt@veit-schiele.de>
2021-06-25 13:08:43 +0200

    Fix #27: RTD theme uses two h1 tags

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ html_title = 'Cusy Design System 0.1.0'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # html_logo = '_static/images/logo/logo.png'
 # html_favicon = '_static/images/logo/favicon.ico'
