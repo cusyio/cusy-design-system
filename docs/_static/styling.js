a23f9204d175acddba15365fe8ac27dd40cf1263
Maris Nieuwenhuis <s88119@bht-berlin.de>
2025-02-27 17:52:05 +0100

    :bug: Fix bokeh textures not showing in firefox

--- docs/_static/styling.js
+++ docs/_static/styling.js
@@ -31,4 +31,62 @@ function styleBokehPlot() {
     }
 }
 
-window.onload = () => styleBokehPlot();
+function fixBokehSvgPatternsInFirefox() {
+  if (navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
+    return;
+  }
+
+  function fixPatternsInSvg(svg) {
+    if (!svg) return;
+
+    const patterns=svg.querySelectorAll('pattern');
+    const measurements=[
+      {width:"130"},
+      {width:"212"},
+      {width:"87"},
+      {width:"118"},
+      {width:"150"},
+      {width:"100"},
+      {width:"150"},
+      {width:"260"},
+    ]
+
+    patterns.forEach((pattern, i) => {
+      const width=pattern.getAttribute('width');
+
+      if (width==='0') {
+        pattern.setAttribute('width', measurements[i].width);
+        pattern.setAttribute('height', "150");
+      }
+    });
+  }
+
+  try {
+    const figureDiv=document.querySelector('div.bk-Figure');
+    if (figureDiv&&figureDiv.shadowRoot) {
+      const canvasDiv=figureDiv.shadowRoot.querySelector('div.bk-Canvas');
+      if (canvasDiv&&canvasDiv.shadowRoot) {
+        const layers=canvasDiv.shadowRoot.querySelectorAll('div.bk-layer');
+        if (layers&&layers.length>1&&layers[1].shadowRoot) {
+          const svgs=layers[1].shadowRoot.querySelectorAll('svg');
+          svgs.forEach(svg=>fixPatternsInSvg(svg));
+        }
+      }
+    }
+  } catch (e) {
+    console.error("Error in fixing SVG:",svg);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', fixBokehSvgPatternsInFirefox);
+document.addEventListener('DOMContentLoaded', styleBokehPlot);
+
+const observer=new MutationObserver(() => {
+  setTimeout(fixBokehSvgPatternsInFirefox, 100);
+  setTimeout(styleBokehPlot, 100);
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
